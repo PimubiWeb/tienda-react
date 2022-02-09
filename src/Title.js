6034c6c8be3600e2c4593e936171cfb00b250f37
Pablo Mochon <pimubiweb@gmail.com>
2022-02-09 09:10:26 +0100

    header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+// Componente title
+import './Title.css';
+
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Pullo</h1>
+            <div className="Title-Subtitle">E-shop made with React</div>
+    </div>
+  )
+}
+
+export default Title
